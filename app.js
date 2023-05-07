6dd87a6c09de96df402af25b2c950dedef52c368
MiguelAlejandroRodriguezCruz <129399935+MiguelAlejandroRodriguezCruz@users.noreply.github.com>
2023-05-07 12:09:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 80;
+const port = process.env.port || 3000;
 
 //configuraciones - respuestas para el cliente(chrome, explore)
 app.use(express.static('public'));
